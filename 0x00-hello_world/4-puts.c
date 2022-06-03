699f6b67a89eafd51a48268f19e0324b294f099a
CreativeTechET <birukyonas331@gmail.com>
2022-06-03 08:13:21 -0700

    answer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+* main — function
+* Description: describe what the function does 
+* Return: always return 0 / 1 depends on the function
+*/
+
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle
+			);
 	return (0);
 }

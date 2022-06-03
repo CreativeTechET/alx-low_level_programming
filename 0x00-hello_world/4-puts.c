17bac174a7ab9b210cc255682037a10fa5d183fe
CreativeTechET <birukyonas331@gmail.com>
2022-06-03 08:17:28 -0700

    modified:   4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
 * main — function
-* Description: describe what the function does 
+* Description: describe what the function does
 * Return: always return 0 / 1 depends on the function
 */
 
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

ffa76c26277a3fe99bfcde050d224dede47cd9a1
CreativeTechET <birukyonas331@gmail.com>
2022-06-06 12:13:24 -0700

    answer

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,8 +7,10 @@
  */
 
 int main(void) {
-    for (int i = 0; i < 10; i++) {
-        printf("%d", i);
-    }
-    return (0);
+
+	int i = 0;
+	for (;i < 10; i++) {
+		printf("%d", i);
+	}
+	return (0);
 }

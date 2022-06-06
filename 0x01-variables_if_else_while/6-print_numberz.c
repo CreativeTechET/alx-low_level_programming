93b66d1ebe5edf79a0628d6831f077496d4e0a68
CreativeTechET <birukyonas331@gmail.com>
2022-06-06 12:45:39 -0700

    answer

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,14 @@
  * Return: success(0)
  */
 
-int main(void) {
-
+int main(void)
+{
 	int i = 0;
-	for (;i < 10; i++) {
+
+	for (i < 10; i++)
+	{
 		putchar(i + '0');
 	}
+	putchar('\n');
 	return (0);
 }

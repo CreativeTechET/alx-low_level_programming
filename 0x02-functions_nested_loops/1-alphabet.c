e76fe1ea222873313681d18138e557f203387075
CreativeTechET <birukyonas331@gmail.com>
2022-06-07 10:22:06 -0700

    answer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always (0)
  */
-
-void print_alphabet(void);
 
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,4 @@
 
+
+
+void print_alphabet(void);

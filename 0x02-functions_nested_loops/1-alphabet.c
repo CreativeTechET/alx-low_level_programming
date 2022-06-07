6d9c282d05fefc849e15a9c82c628c2fb65f1465
CreativeTechET <birukyonas331@gmail.com>
2022-06-07 10:43:15 -0700

    answer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always (0)
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+        /**
+        * print_alphabet - prints all the alphabets
+        *
+        * Return: a character value
+        */
+
+        int i = 97;
+
+        for (; i < 123; i++)
+        {
+                putchar(i);
+        }
 }

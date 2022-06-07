4157daa8992e5f49fb71937d9a368c2d94557eaf
CreativeTechET <birukyonas331@gmail.com>
2022-06-07 15:57:08 -0700

    answer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 		int i;
 		for (i = n; i >= 98; i++)
 		{
-			printf("%d",i);
+			printf("%d, ",i);
 			_putchar('\n');
 		}
 	}
@@ -18,7 +18,7 @@ void print_to_98(int n)
 		int i;
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d",i);
+			printf("%d, ",i);
 			_putchar('\n');
 		}
 	}

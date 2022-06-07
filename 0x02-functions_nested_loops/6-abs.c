dbf17ea9d8c1354be077f1b0b6679281690eb3b2
CreativeTechET <birukyonas331@gmail.com>
2022-06-07 15:03:33 -0700

    answer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _abs - get the absolute value of integer
+ * @n: number 
+ * Return: computed value
+ */
+
 int _abs(int n)
 {
 	int computed = abs(n);
+
 	return (computed);
 }

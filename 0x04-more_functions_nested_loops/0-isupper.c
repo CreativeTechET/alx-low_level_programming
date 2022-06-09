ddb263f75b4b028e3556f9fc92569e2a38f5826d
CreativeTechET <birukyonas331@gmail.com>
2022-06-09 02:00:23 -0700

    answer

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/*
+ * _isupper - converts a chatr into uppercase char
+ *
+ * Return: the converted char
+ */
+
+int _isupper(int c)
+{
+	char context = isupper(c);
+
+	return (context);
+}

6fb7bf72381a14366e63476aad8ac851034771a7
CreativeTechET <birukyonas331@gmail.com>
2022-06-09 03:15:10 -0700

    answer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * mul - multiplies the 2 numbers
+ * mul - function multiplies the  numbers.
  * @a: first number
  * @b: secound number
+ * Return: the muliplied number
  */
 
 int mul(int a, int b)
 {
 	int times = a * b;
+
 	return (times);
 }
 

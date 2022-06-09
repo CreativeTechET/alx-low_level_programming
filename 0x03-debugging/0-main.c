161d27d5f21f8caddc2953cb195adc50baa08a51
CreativeTechET <birukyonas331@gmail.com>
2022-06-09 07:10:33 -0700

    answer

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -12,18 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
 	return (0);
 }

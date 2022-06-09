d724b87d4b63ca5a69e3dad293aaf86de0806662
CreativeTechET <birukyonas331@gmail.com>
2022-06-09 06:34:26 -0700

    answer

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * print_line - print a blank line
+ * @n: parameter for how many lines
+ */
 
 void print_line(int n)
 {
-	if (n <= 0 ) {
+	if (n <= 0)
+	{
 		_putchar('\n');
 	}
-	else {
+	else
+	{
 		int i;
 
 		for (i = 0; i <= n; i++)

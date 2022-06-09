5caa499a75d1a84f2da2719bf3777584f286a783
CreativeTechET <birukyonas331@gmail.com>
2022-06-09 03:45:26 -0700

    answer

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints out most of the numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}

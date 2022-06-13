87c5cc021dd6e813d6899baa6f3ac10962ef73e2
CreativeTechET <birukyonas331@gmail.com>
2022-06-13 00:20:30 -0700

    answer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset the number by reference pointer
+ * @n: pointer parameter
+ * Return: the reseted number
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (n);
+}

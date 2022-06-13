f91284e4a8be2a5903c1630dd3933143cab87e96
CreativeTechET <birukyonas331@gmail.com>
2022-06-13 14:00:36 -0700

    answer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,17 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i <= (n - 1); i++)
 	{
 		if (i == (n - 1))
 		{
-			printf("%i",a[i]);
+			printf("%i", a[i]);
 		}
 		else
 		{
 
-			printf("%i, ",a[i]);
+			printf("%i, ", a[i]);
 		}
 	}
 		printf("\n");

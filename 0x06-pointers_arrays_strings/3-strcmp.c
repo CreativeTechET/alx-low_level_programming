90ff333ae39a2db00292ef61e9b84ae68e613f3e
CreativeTechET <birukyonas331@gmail.com>
2022-06-16 02:44:28 -0700

    answer

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpm - function to compare two strings
+ * @ds1: first destination
+ * @s2: secound destination
+ * Return: return  0 or 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	chat *tmp = strcpm(s1, s2);
+
+	return (rmp);
+}

e62c434a4b8d90ed38e2a23faf2947f0ccc0ba13
CreativeTechET <birukyonas331@gmail.com>
2022-06-16 01:38:57 -0700

    answer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,30 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strcat -  concatinates the string each other
- * Returns: a char with the final string
+ * @dest: destination
+ * @src: source
+ * Return: a char with the final string
  */
-
-char final[100];
 
 char *_strcat(char *dest, char *src)
 {
-	int  i,j;
-
-	for (i = 0; src[i] != '\0'; i++)
+	int  i, j,len;
+	
+	len = strlen(src);
+	j = 0;
+	for (; j < len; j++)
 	{
-		final[i] = src[i];
+		char tmp = src[j];
+		if (tmp == '\0')
+		{
+			i = 0;
+			while (dest[i] != '\0')
+			{
+				src[j] = dest[i];
+			}
+		}
 	}
-	for (j = 0; dest[j] != '\0'; j++)
-	{
-		final[i] = dest[j];
-	}
-	return (final);
+	return (dest);
 }

376c0172d36c06ba7542f95af961dbeafde0980e
CreativeTechET <birukyonas331@gmail.com>
2022-06-22 07:06:38 -0700

    answer

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,7 +8,6 @@
 
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s);
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 void _putchar(char c);
+void _print_rev_recursion(char *s);
+
 
 #endif

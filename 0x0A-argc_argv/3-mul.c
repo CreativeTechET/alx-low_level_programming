0b562b62f0ad1721f545721f80281f9d2888771d
CreativeTechET <birukyonas331@gmail.com>
2022-06-24 00:09:37 -0700

    answer

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argv: addtional parameter for the program
+ * @argc: length (size) of the argv
+ * Return: (0) success
+ */
 
 int main(int argc, char *argv[])
 {
-	if (argv[1] && argv[2])
+	if ((argv[1] && argv[2]) || argc == 3)
 	{
-		int res = argv[1] * argv[2];
+		int res = atoi(argv[1]);
+		int res2 = atoi(argv[2]);
 
-		printf("%d", res);
+		printf("%d\n", res * res2);
 		return (0);
 	}
 	else

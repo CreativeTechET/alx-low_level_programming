9d95a9c28fb804449febda79d6458ebd8e61c095
CreativeTechET <birukyonas331@gmail.com>
2022-06-24 07:38:07 -0700

    answer

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +21,8 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif

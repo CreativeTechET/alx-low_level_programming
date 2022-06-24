db35a4e1ed7c11bd2016585802fd15c1ec5d74d6
CreativeTechET <birukyonas331@gmail.com>
2022-06-23 23:20:12 -0700

    answer

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argv: addtional parameter for the program
+ * @argc: length (size) of the argv
+ * Return: (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}

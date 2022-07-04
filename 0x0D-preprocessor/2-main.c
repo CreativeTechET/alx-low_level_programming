4ea7797d8ecf57c6c61ea236fbc95f905d67abf1
CreativeTechET <birukyonas331@gmail.com>
2022-07-04 13:34:54 -0700

    answer

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define _FILE_
+
+/**
+ * main - Entry POint
+ * @argc: size of arg
+ * @argv: array of arg
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

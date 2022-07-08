644b708552dfe8fa90533e7936945ff1d080d8ac
CreativeTechET <birukyonas331@gmail.com>
2022-07-07 17:20:21 -0700

    answer

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function to add all args together
+ * @n: number of args
+ * Return: the final int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int res = 0;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		res += va_arg(args, int);
+	}
+	va_end(args);
+	return (res);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif

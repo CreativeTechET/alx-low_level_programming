5d6b10cbe018e80bdacf7ece78eaf24c549d8d3d
CreativeTechET <birukyonas331@gmail.com>
2022-07-07 17:45:06 -0700

    answer

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function to print numbers
+ * @separator: separator char
+ * @n: number of arg
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int res;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+
+		res = va_arg(args, int);
+		printf("%d", res);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s ", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,10 @@
 #define MAIN
 
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 
 #endif

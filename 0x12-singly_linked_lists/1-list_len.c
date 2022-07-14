d9425c252f1227ed22f22da1ec2f542b3ec50c65
CreativeTechET <birukyonas331@gmail.com>
2022-07-14 14:09:38 -0700

    print out number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * list_len - function to print out the number of elements
+ * @h: linked ist head
+ * Return: the number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != 0)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}

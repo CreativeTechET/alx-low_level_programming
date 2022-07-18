00b6bbaf62df4c29059371dd59a98042ee8175fb
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-19 00:53:57 +0300

    get nth index of node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,21 +1,61 @@
 #include "lists.h"
-#include <stdlib.h>
+/**
+ * listint_len - returns the number of elements ina linked listint_t list.
+ * @h: pointer to first element on list.
+ *
+ * Return: number of elements in list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count;
+	
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
 /**
- * get_nodeint_at_index - Find a node in a list.
- * @head: Address of the first node in a list.
- * @index: Position of a the node to find
- * Return: Node address
+ * get_nodeint_at_index - returns nth node of a listint_t linked list.
+ * @head: pointer to first elemnt of list.
+ * @index: index of node.
+ *
+ * Return: nth node,
+ * NULL if not found.
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int y = 0;
-
-	if (head == NULL)
-	head = head->next;
-		if (head == NULL)
-			return (NULL);
+	listint_t *current;
+	unsigned int count;
+	
+	current = head;
+	count = listint_len(head);
+	
+	if (current == NULL)
+		count++;
+	
+	if (count <= index)
+	{
+		return (NULL);
 	}
-	return (head);
+	else
+	{
+		count = 0;
+		while (current != NULL)
+		{
+			if (count == index)
+			{
+				break;
+			}
+			else
+			{
+				count++;
+				current = current->next;
+			}
+		}
+		return (current);
+	}
 }

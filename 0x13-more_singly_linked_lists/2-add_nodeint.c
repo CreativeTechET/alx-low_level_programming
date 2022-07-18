ab5cef198f1e7fa19281587fc38a795f373ce749
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-18 22:55:27 +0300

    add a new node at the beggining

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_nodeint - add a new node in the beggining
+ * @head: pointer to the head of the linked list
+ * @n: data to add to the node
+ * Return: the memory address of the new linked list head
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = NULL;
+
+	new->next = *head;
+	*head = new;
+	return (*head);
+}

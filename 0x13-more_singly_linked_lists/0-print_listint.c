9eb82770ee2451b65d796d574b5492d6a75090bd
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-18 21:14:37 +0300

    print all list in the lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,21 +8,17 @@
 
 size_t print_listint(const listint_t *h)
 {
-
 	size_t count = 0;
-		if (h == NULL)
+	
+	if (h == NULL)
 	{
 		return (0);
 	}
-	else
+	while (h)
 	{
-		while (h)
-		{
-			count++;
-			printf("%d\n", h->n);
-			h = h->next;
-
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }

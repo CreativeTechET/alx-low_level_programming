722362ce1bb0f544e813450ef62615f62bf1e373
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-27 16:39:24 +0300

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,47 @@
 #include "main.h"
 
+/**
+ * read_textfile - read a text file and print it to stdout
+ * @letters: the number of letters to read
+ * @filename: The name of the file to read.
+ * Return: 0 on success, -1 on error.
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fp;
-    size_t r,f;
-    char *buffer;
+	int fp;
+	size_t r, f;
+	char *buffer;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    buffer = malloc(letters * sizeof(char) + 1);
-    if (buffer == NULL)
-    {
-        return (EXIT_FAILURE);
-    }
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(letters * sizeof(char) + 1);
+	if (buffer == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+	fp = open("Requiescat", O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
 
-    fp = open("Requiescat",O_RDONLY);
-    if (fp == -1)
-    {
-        return (0);
-    }
-    
-    r = read(fp,buffer,letters);
-    if ((int)r == -1)
-    {
-        return (0);
-    }
-    buffer[letters] = '\0';
-    
-    f = write(STDOUT_FILENO,buffer,r);
-    if ((int)f == -1)
-    {
-        return (0);
-    }
-    free(buffer);
-    close(fp);
+	r = read(fp, buffer, letters);
+	if ((int)r == -1)
+	{
+		return (0);
+	}
+	buffer[letters] = '\0';
 
-
-    return (f);
+	f = write(STDOUT_FILENO, buffer, r);
+	if ((int)f == -1)
+	{
+		return (0);
+	}
+	free(buffer);
+	close(fp);
+	return (f);
 }
 

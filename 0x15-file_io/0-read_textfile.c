e2f0e0b4f64bdf4224784abfa4277723a0837ffe
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-27 16:55:43 +0300

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,39 +9,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	size_t r, f;
-	char *buffer;
+	int file, rd, wr;
+	char *buf;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-	buffer = malloc(letters * sizeof(char) + 1);
-	if (buffer == NULL)
-	{
-		return (EXIT_FAILURE);
-	}
-	fp = open(filename, O_RDONLY);
-	if (fp == -1)
-	{
-		return (0);
-	}
 
-	r = read(fp, buffer, letters);
-	if ((int)r == -1)
-	{
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
-	}
-	buffer[letters] = '\0';
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
 
-	f = write(STDOUT_FILENO, buffer, r);
-	if ((int)f == -1)
-	{
+	buf[letters] = '\0';
+
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
 		return (0);
-	}
-	free(buffer);
-	close(fp);
-	return (f);
+
+	close(file);
+	free(buf);
+	return (wr);
 }
 

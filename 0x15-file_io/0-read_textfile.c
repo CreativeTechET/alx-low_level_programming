a6b4ee3269b77d567a30035dbc4cd1a44c0316c8
Buka <birukyonas331@gmail.com>
2022-07-27 16:21:58 +0300

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,13 +4,17 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fp;
-    size_t r;
-    char buffer[1000];
+    size_t r,f;
+    char *buffer;
 
-    
     if (filename == NULL)
     {
         return (0);
+    }
+    buffer = malloc(letters * sizeof(char) + 1);
+    if (buffer == NULL)
+    {
+        return (EXIT_FAILURE);
     }
 
     fp = open("Requiescat",O_RDONLY);
@@ -18,13 +22,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
     {
         return (0);
     }
-
+    
     r = read(fp,buffer,letters);
-    fprintf(stdout,buffer,letters);
-
+    if ((int)r == -1)
+    {
+        return (0);
+    }
+    buffer[letters] = '\0';
+    
+    f = write(STDOUT_FILENO,buffer,r);
+    if ((int)f == -1)
+    {
+        return (0);
+    }
+    free(buffer);
     close(fp);
 
 
-    return r;
+    return (f);
 }
 

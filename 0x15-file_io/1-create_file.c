e4312f5d07a21fbe93573b625e7a8bd52e8daea6
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-29 14:55:09 +0300

    create a file to write to

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (FAILED);
 
-	file = open(filename, O_RDWR || O_CREAT, O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (FAILED);
 

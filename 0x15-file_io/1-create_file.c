1e48ad7246488bbf242299aa2d59076b84687c40
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-07-29 14:23:39 +0300

    create file using open()

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
+/**
+ * create_file - creates a new file or truncates it if it exist to write to it
+ * @filename: name of the file to create
+ * @text_content: content to write to the file
+ * Return: 1 if successful, -1 otherwise
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-	int file, i,add;
+	int file, i, add;
 
 	if (filename == NULL)
-		return FAILED;
+		return (FAILED);
 
 	file = open(filename, O_RDWR || O_CREAT, O_TRUNC, 0600);
 	if (file == -1)
-		return FAILED;
+		return (FAILED);
 
 	if (text_content)
 	{
@@ -18,10 +25,10 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
-		add = write(file,text_content,i);
+		add = write(file, text_content, i);
 		if (add != i)
-			return FAILED;
+			return (FAILED);
 	}
 	close(file);
-	return (1);
+	return (SUCCESS);
 }
